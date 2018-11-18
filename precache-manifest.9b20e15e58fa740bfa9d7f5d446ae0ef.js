6330fe971ae3277ef09b7c75ef807fe3081e7b02
Brian Mejia <Bmejia319@gmail.com>
2018-11-18 16:42:53 -0500

    Updates

---
+++ precache-manifest.9b20e15e58fa740bfa9d7f5d446ae0ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "12bb5720a3108618088b",
+    "url": "/clicky-game/static/js/main.12bb5720.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/clicky-game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "12bb5720a3108618088b",
+    "url": "/clicky-game/static/css/main.5c38fced.chunk.css"
+  },
+  {
+    "revision": "368991c84067eacbd729f8d3f6997e05",
+    "url": "/clicky-game/index.html"
+  }
+];
